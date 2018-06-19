a0e14404165f839821fa3098e1c283d29c2bf003
Mark Haferkamp <refola@users.noreply.github.com>
2018-06-19 13:52:03 -0700

    use swipl predicates one-liners where possible

--- about_lists.pl
+++ about_lists.pl
@@ -12,33 +12,40 @@ s(X,Y) :- X #= Y + 1.
 % X is the head of the list.
 my_first(X,[X|_]).
 
+/* Use "%" at the start of this line for manual implementation.
+%% my_last(LastElementOfList, List).
 % X is the tail (of the tail (of the tail (...))) of the list.
 my_last(X, [X]).
 % If there are multiple items, just keep chopping off the head.
 my_last(X, [_|T]) :-
     my_last(X, T).
+%*/ my_last(X, Xs) :- last(Xs, X).
 
 % Same as above, only stop one step sooner.
 my_penultimate(X, [X,_]).
 my_penultimate(X, [_|Rest]) :-
     my_penultimate(X, Rest).
 
+/*
 % At position 1, X is the head of the list.
 my_element_at(X, [X|_],1).
-
 % At position n, X is at position n-1 of the list's tail.
 my_element_at(X, [_|Rest],N) :-
     N > 1, % How necessary is this for correctness? Is it just to
            % avoid infinite loops if N<1 is passed?
     N1 is N-1,
     my_element_at(X, Rest, N1).
+%*/ my_element_at(X, Xs, I) :- nth1(I, Xs, X).
 
+/*
 % Count how many elements are in a list.
 my_number_of(0,[]).
 my_number_of(N,[_|T]) :-
     my_number_of(M,T),
     s(N,M).
+%*/ my_number_of(N,Xs) :- length(Xs, N).
 
+/*
 % Reverse a list. Base case: Singleton lists are already reversed.
 my_reverse([X], [X]) :- true.
 % Recursive case: Appending the head to the reverse of the tail yields
@@ -47,11 +54,13 @@ my_reverse(L1,L2) :-
     [H1 | T1] = L1,
     my_reverse(T1, T2),
     append(T2, [H1], L2).
+%*/ my_reverse(Xs, Ys) :- reverse(Xs, Ys).
 
 % Is the list the same when reversed?
 is_palindrome(X) :-
     my_reverse(X, X).
 
+/*
 %% Flatten a list X such that the output Flat f has the same order of
 %% underlying non-list elements but in a single list.
 % An empty list goes to itself.
@@ -65,8 +74,10 @@ my_flatten([H|T], Flat) :-
     my_flatten(H, FlatH),
     my_flatten(T, FlatT),
     append(FlatH, FlatT, Flat).
+%*/ my_flatten(Xs, Flat) :- flatten(Xs, Flat).
 
-my_compress(_,_) :- false.
+% Remove all but the first in runs of duplicate elements.
+my_compress(Orig,Deduped) :- false.
 
 my_pack(_,_) :- false.
 
